664cade3da371a4d9eeface486944534cccee776
Antonio Jesús Sánchez Padial <antonio.sanchez@inia.es>
2014-09-11 13:45:44 +0200

    Improve names

--- cleanCache.php
+++ cleanCache.php
@@ -1,9 +1,11 @@
 <?php
-function clean($list, $types)
+function clean(array $dir_list, array $types_to_clean)
 {
-    foreach ($list as $element) {
-        if (is_file($element)) && in_array($types, strrchr($element, '.')) {
-            unlink($element);
+    foreach ($dir_list as $element) {
+        if (is_file($element)) {
+	    if (in_array($types_to_clean, $filetype($element))) {
+                unlink($element);
+	    }
         }
     }
 }
